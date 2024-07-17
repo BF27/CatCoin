dffc0f011d690afe6b4f1c969fcd26a679147b4b
Farkas Bence <farkas.bence27@gmail.com>
2024-07-18 01:08:21 +0200

    add navigation between login and root page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,25 @@
 import InputField from "../components/Form/InputField";
 import Button from "../components/Button";
+import Link from "next/link";
 
 export default function Login() {
   return (
     <form>
-      <InputField id="loginId" type="email" placeholder="pelda@pelda.hu" labelText="Adja meg Email címét"/>
-      <InputField id="loginPw" type="password" placeholder="jelszo" labelText="Adja meg a jelszavát" />
-      <Button text="Bejelentkezés" />
+      <InputField
+        id="loginId"
+        type="email"
+        placeholder="pelda@pelda.hu"
+        labelText="Adja meg Email címét"
+      />
+      <InputField
+        id="loginPw"
+        type="password"
+        placeholder="jelszo"
+        labelText="Adja meg a jelszavát"
+      />
+      <Link href="/">
+        <Button text="Bejelentkezés" />
+      </Link>
     </form>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Button from "./components/Button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Üdvözöl a CatCoin APP!!</h1>
+    <div>
+      <h1>Üdvözöl a CatCoin APP!!</h1>
+      <Link href="/login">
+        <Button text="Tovább a bejelentkezéshez" />
+      </Link>
+    </div>
   );
 }
